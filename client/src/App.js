a999ec8cbfacfd3bbea07152a658d06fa3cc7edd
ehpc <ehpc@ehpc.io>
2020-05-21 09:54:42 +0300

    Add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, {useEffect} from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import Home from './Home';
+import Test from './Test';
 import './App.css';
 
 function App() {
@@ -11,22 +13,22 @@ function App() {
     })();
   });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <main>
+        <Switch>
+          <Route path="/test">
+            <Test />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/test">Test</Link>
+      </nav>
+    </Router>
   );
 }
 
