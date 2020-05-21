5a8f6ac406a8854b218d30d783d99138ee4be14d
ehpc <ehpc@ehpc.io>
2020-05-21 09:37:04 +0300

    Add build

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
+
+app.use(express.static(path.resolve('../client/build/')));
 
 app.get('/api/test', (req, res) => res.send('Wasssssuuup'));
 
